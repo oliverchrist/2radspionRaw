9c4f02d416c34cf24f56ac44995ce9237fa5a56f
Oliver Christ <oliver.christ@mediaman-technology.de>
2012-02-02 23:49:23 +0100

    phpinfo

---
+++ phpinfo.php
@@ -0,0 +1,11 @@
+<?php include 'includes/head.php'; ?>
+<body id="std">
+    <?php include 'includes/header.php'; ?>
+	<div id="content">
+     <?php
+     phpinfo();
+     ?>
+    </div>
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>
