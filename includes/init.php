a22b2b0d4e82f7e81734e55e762de48d1068bb7d
Oliver Christ <oliver.christ@mediaman-technology.de>
2012-03-21 23:32:25 +0100

    twigify

--- includes/init.php
+++ includes/init.php
@@ -7,4 +7,12 @@ function __autoload($className){
 }
 
 session_start();
+
+require_once '/usr/share/php/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+  'cache' => 'compilation_cache',
+));
 ?>
