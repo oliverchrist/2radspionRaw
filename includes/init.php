23c97023accb62c5f82d4c33b8f4400041bf97d6
Oliver Christ <oliver.christ@mediaman-technology.de>
2012-03-24 21:12:52 +0100

    geht doch

--- includes/init.php
+++ includes/init.php
@@ -4,11 +4,11 @@ function __autoload($className){
     #echo "load Class: $className<br>";
 
     if (0 !== strpos($className, 'Twig')) {
-	    $fileName = 'includes/' . str_replace('\\', '/', $className) . '.php';
+        $fileName = 'includes/' . str_replace('\\', '/', $className) . '.php';
     }else if (is_file($fileName = '/usr/share/php/'.str_replace(array('_', "\0"), array('/', ''), $className).'.php')) {
         require $fileName;
     }
-	
+
     require_once $fileName;
 }
 
@@ -18,11 +18,11 @@ require_once '/usr/share/php/Twig/Autoloader.php';
 Twig_Autoloader::register();
 */
 
-/*
+
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array(
   'cache' => 'compilation_cache',
 ));
-*/
+
 
 ?>
