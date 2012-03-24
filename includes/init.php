8531fbb3db73d7c92b3aa13a76d91f97dd60037b
Oliver Christ <oliver.christ@mediaman-technology.de>
2012-03-24 21:07:28 +0100

    loader

--- includes/init.php
+++ includes/init.php
@@ -1,18 +1,28 @@
 <?php
 include 'includes/properties/properties.php';
 function __autoload($className){
-    $fileName = 'includes/' . str_replace('\\', '/', $className) . '.php';
-    #echo "load Class: $fileName<br>";
-    require $fileName;
+    #echo "load Class: $className<br>";
+
+    if (0 !== strpos($className, 'Twig')) {
+	    $fileName = 'includes/' . str_replace('\\', '/', $className) . '.php';
+    }else if (is_file($fileName = '/usr/share/php/'.str_replace(array('_', "\0"), array('/', ''), $className).'.php')) {
+        require $fileName;
+    }
+	
+    require_once $fileName;
 }
 
 session_start();
-
+/*
 require_once '/usr/share/php/Twig/Autoloader.php';
 Twig_Autoloader::register();
+*/
 
+/*
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array(
   'cache' => 'compilation_cache',
 ));
+*/
+
 ?>
